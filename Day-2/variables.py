156d777da4b0fa3fa4f0c743c3a24b942c1ab377
usharanikadapa7-pixel <usharanikadapa7@gmail.com>
2026-05-27 17:48:03 +0530

    Add files via upload

---
+++ Day-2/variables.py
@@ -0,0 +1,50 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+===================================================== RESTART: C:/Users/usha1/OneDrive/Desktop/python-course-work/Day-2/keywords.py ====================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> myvar=10
+>>> myvar
+10
+>>> Myvar=10
+>>> MYvar=10
+>>> Myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> Myvar=10
+>>> _myvar=10
+>>> 2myvar=10
+SyntaxError: invalid decimal literal
+>>> @myvar=10
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> if=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> Myvar
+10
+>>> myvar=20
+>>> myvariable=20
+>>> Myvariable
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    Myvariable
+NameError: name 'Myvariable' is not defined. Did you mean: 'myvariable'?
+>>> a=10
+>>> a=b=c=20
+>>> b
+20
+>>> c
+20
+>>> a=30
+>>> b=40
+>>> a,b=b,c
+>>> a
+40
+>>> b
+20
+>>> c
+20
